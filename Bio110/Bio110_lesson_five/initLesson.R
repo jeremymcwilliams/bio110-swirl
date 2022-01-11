799efea1353d1850374d90ca7bd7a0050107f260
Jeremy McWilliams <jeremym@lclark.edu>
2022-01-10 16:54:12 -0800

    add main files

---
+++ Bio110/Bio110_lesson_five/initLesson.R
@@ -0,0 +1,16 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+
+      .get_course_path <- function(){
+        tryCatch(swirl:::swirl_courses_dir(),
+                 error = function(c) {file.path(find.package("swirl"),"Courses")}
+        )
+      }
+
+      data <- read.csv(file.path(.get_course_path(), "Bio110", "Bio110_lesson_five", "physiology_data.csv"))
+
+
+swirl_options(swirl_logging = TRUE)
---
+++ Bio110/Bio110_lesson_one/initLesson.R
@@ -0,0 +1,13 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+swirl_options(swirl_logging = TRUE)
+
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+data <- read.csv(file.path(.get_course_path(), "Bio110", "Bio110_lesson_one", "physiology_data.csv"))
